e11e19dcea4bc676393a16d1252519e19510ab55
Che1 <nachwon@gmail.com>
2017-11-08 19:42:01 +0900

    app started

--- rest_practice/config/settings.py
+++ rest_practice/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'rest_practice.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'rest_practice.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
